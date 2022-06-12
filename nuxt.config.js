8c35062460bb7b423ffe1af30e3fc99b54f1c10d
shouzia <saopipizhu@163.com>
2022-06-12 11:38:40 +0800

    更新为静态文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,5 +42,19 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  // 自定义配置路由
+router: {
+  mode: 'hash', // 使用 'hash' 主要是为了适配以相对路径打开的静态站点， 必须使用 'hash' 否则路由跳转不生效
+  // base: '/moli/',
+  base: process.env.NODE_ENV === 'production' ? './' : '/', // 使用 './' 主要是为了适配以相对路径打开的静态站点
+  extendRoutes(routes, resolve) {
+   ({
+    path: '/',
+    redirect: {
+     name: 'home'
+    }
+   })
   }
+ },
 }
