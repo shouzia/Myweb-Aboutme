e090ce0574e3eabc5e471feddef17839379634b2
shouzia <saopipizhu@163.com>
2022-12-07 21:14:24 +0800

    💄 删除灰色

--- pages/index.vue
+++ pages/index.vue
@@ -16,15 +16,7 @@ export default {
 </script>
 
 <style>
-.Index {
-  -webkit-filter: grayscale(100%);
-  -moz-filter: grayscale(100%);
-  -ms-filter: grayscale(100%);
-  -o-filter: grayscale(100%);
-  filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
-  _filter: none;
-  filter: grayscale(100%);
-}
+
 .Bg-index {
   z-index: -1;
   position: fixed;
