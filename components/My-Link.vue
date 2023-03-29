e61b2a35906ce6f90dcc14d6e76a8ba8b3295e89
shouzia <saopipizhu@163.com>
2023-03-29 17:26:50 +0800

    📦 添加又拍云 和备案信息

--- components/My-Link.vue
+++ components/My-Link.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="My-Link">
     <div class="div-Icon">
-      <a
-        v-for="index in Link"
-        :key="index.icon"
-        :href="index.link"
-        class="Icon-style"
-        target="_blank"
-      >
+      <a v-for="index in Link" :key="index.icon" :href="index.link" class="Icon-style" target="_blank">
         <b-icon :icon="index.icon"></b-icon>
       </a>
     </div>
@@ -15,11 +9,18 @@
     <div class="foot">
       <p>
         &copy;2019-{{ new Date().getFullYear() }}<span>|</span>
-        <a class="HYlink" target="_blank" href="https://blog.shouzi.xyz"
-          >瘦子Blog</a
-        >
+        <a class="HYlink" target="_blank" href="https://blog.shouzi.xyz">瘦子Blog</a>
         <span>|</span>
         <a class="HYlink" target="_blank" href="https://hypin.cn">幻影官网</a>
+      </p>
+      <p style="margin-top: 40px;">
+        <a class="HYlink" href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral">
+          <img src="@/assets/Img/upyun.png" width="78px" height="26px" alt="">
+        </a>
+        <span>|</span>
+        <a class="HYlink" style="color: white;" href="https://beian.miit.gov.cn/" target="_blank">
+          赣ICP备2023002713号
+        </a>
       </p>
     </div>
   </div>
@@ -58,46 +59,60 @@ export default {
   color: white;
   text-align: center;
 }
+
 a {
   font-size: 2.1429rem;
   text-decoration: none;
   color: rgba(255, 255, 255, 0.6);
 }
+
 a:hover {
   text-decoration: none;
   color: #5cbbf6;
 }
+
 .HYlink {
   font-size: 1.2286rem;
   text-decoration: none;
 }
+
 .HYlink:hover {
   color: #5cbbf6;
 }
+
 .Icon-style {
   font-size: 20px;
   margin: 0 15px;
 }
+
 .div-Icon {
   margin: 30px 0;
 }
+
 .foot {
   font-size: 1.2286rem;
 }
+
 span {
   margin: 0px 20px;
 }
+
 @media screen and (max-width: 800px) {
   .foot {
     font-size: 1.1286rem;
   }
+
   .HYlink {
     font-size: 1.1286rem;
     text-decoration: none;
   }
+
   .Icon-style {
     margin: 0 15px;
     font-size: 20px;
   }
 }
-</style>
+
+.HYlink img {
+  object-fit: cover;
+}</style>
