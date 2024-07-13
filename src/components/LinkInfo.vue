e0db01874a4ff60d08e4b9d7f84a4c96fd090c62
shouzia <saopipizhu@163.com>
2024-07-13 18:42:46 +0800

    💄修改样式 修改又拍云联盟

--- src/components/LinkInfo.vue
+++ src/components/LinkInfo.vue
@@ -1,23 +1,18 @@
 <template>
-  <div class="flex justify-between w-full items-center">
-    <div>
-      <a
-        href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img class="h-[48px]" src="@/assets/upy.png" alt="upy" />
+  <div class="flex flex-col justify-center w-full">
+    <div class="w-full flex justify-center items-center">
+      <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" target="_blank"
+        class="flex justify-center items-center" rel="noopener noreferrer">
+        本网站由
+
+        <img class="h-[48px] mx-[10px]" src="@/assets/upy.png" alt="upy" />
+        提供 CDN 加速/云存储服务
       </a>
+
     </div>
-    <div>|</div>
-    <div>
-      <a
-        class="hover:text-[#0099FF]"
-        href="https://beian.miit.gov.cn/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >赣ICP备2023002713号</a
-      >
+    <div class="w-full flex justify-center items-center mt-[20px]">
+      <a class="hover:text-[#0099FF]" href="https://beian.miit.gov.cn/" target="_blank"
+        rel="noopener noreferrer">赣ICP备2023002713号</a>
     </div>
   </div>
 </template>
